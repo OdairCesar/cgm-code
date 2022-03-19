860c4ae5f3b085b3f3abf2203feacac1b6d20280
Odair Cesar Ferreira <odairferreira97@gmail.com>
2022-03-19 06:25:42 -0300

    Criando rodapé e seção de contato

---
+++ src/components/layout/contato/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./index.css";
+
+import imgContato from "../../../assets/img/image10.png";
+import Icone from "../../template/icone";
+
+export default function Contato() {
+  return (
+    <section className="contato py-5">
+      <div className="container d-flex">
+        <div className="d-flex flex-column col-6 py-4 pe-4">
+          <div className="titulo-contato">
+            <h4>Ficou com dúvidas?</h4>
+            <h2>Entre em contato</h2>
+          </div>
+
+          <form className="contato-formulario d-flex flex-column">
+            <input type="text" placeholder="Nome"></input>
+            <input type="email" placeholder="E-mail"></input>
+            <input type="number" placeholder="Telefone/Whatsapp"></input>
+            <textarea className='p-2' placeholder="Mensagem"></textarea>
+            <button className='buttonFormulario d-flex justify-content-center'>
+              Enviar
+              <Icone cssBootstrap='ms-1' nameIcone='arrow-right'/>
+            </button>
+          </form>
+        </div>
+
+        <img className="col-6" src={imgContato} alt="Imagem Contato" />
+      </div>
+    </section>
+  );
+}
---
+++ src/components/layout/footer/index.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import "./index.css";
+
+import imgLogo from "../../../assets/img/logo2.png";
+import Icone from "../../template/icone";
+
+export default function Footer() {
+  return (
+    <>
+      <section className="info-extra text-white">
+        <div className="container d-flex py-5 justify-content-between">
+          <img src={imgLogo} alt="Logo do rodapé" />
+
+          <div className="paginas pt-4">
+            <p>Início</p>
+            <p>Sobre</p>
+            <p>Soluções</p>
+            <p>Contatos</p>
+          </div>
+
+          <div className="info-contato pt-4">
+            <p>Informações</p>
+            <p className="d-flex h5">
+              <Icone cssBootstrap='me-2' nameIcone="facebook" />
+              (00) 0000-0000
+            </p>
+            <p className="d-flex h5">
+              <Icone cssBootstrap='me-2' nameIcone="facebook" />
+              contato@cgm.com.br
+            </p>
+            <p className="d-flex h5">
+              <Icone cssBootstrap='me-2' nameIcone="facebook" />
+              Seg. a sex. das 9h às 18h
+            </p>
+          </div>
+
+          <div className="info-redesocial d-flex justify-content-end align-items-center col-3 text-center">
+            <Icone cssBootstrap='bg-white mx-1 rounded-circle' nameIcone="facebook" />
+            <Icone cssBootstrap='bg-white mx-1 rounded-circle' nameIcone="linkedin" />
+            <Icone cssBootstrap='bg-white mx-1 rounded-circle' nameIcone="instagram" />
+          </div>
+        </div>
+      </section>
+
+      <section className="copy">
+        <div className="container">
+          <p>CGM @copy 2022 - Todos os direitos reservados</p>
+          <img />
+        </div>
+      </section>
+    </>
+  );
+}
