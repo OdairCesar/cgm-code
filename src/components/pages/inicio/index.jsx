2262a53e384a1a2b57e499377e4cb6b2a2d8edb6
Odair Cesar Ferreira <odairferreira97@gmail.com>
2022-03-21 13:15:55 -0300

    reajustes

--- src/components/pages/inicio/index.jsx
+++ src/components/pages/inicio/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./index.css";
 import Contato from "../../layout/contato";
@@ -10,13 +11,12 @@ import imgPessoaFisica from "../../../assets/img/image4.png";
 import imgPessoaJurídica from "../../../assets/img/image6.png";
 import imgHistoria from "../../../assets/img/image8.png";
 import TituloDois from "../../template/tituloDois";
-import { Link } from "react-router-dom";
 
 export default function Inicio() {
   return (
     <>
       {/*================= BANNER/INICIO =====================*/}
-      <section id='top' className="banner-inicio">
+      <section id="top" className="banner-inicio">
         <div className="banner-inicio-conteudo container d-flex align-items-end">
           <div className="d-flex flex-column pb-5">
             <h1 className="text-white fw-bold">
@@ -47,15 +47,23 @@ export default function Inicio() {
 
           <div className="d-flex flex-column flex-lg-row align-items-center justify-content-between">
             <div className="cardApresentacao d-flex flex-column align-items-center col-12 col-md-6 p-3">
-              <img className="w-100" src={imgPessoaFisica} alt="Sou Pessoa Fisica" />
+              <img
+                className="w-100"
+                src={imgPessoaFisica}
+                alt="Sou Pessoa Fisica"
+              />
               <button className="d-flex">
-                <Link to='/fisico'>Sou Pessoa Física</Link>
+                <Link to="/fisico">Sou Pessoa Física</Link>
                 <Icone cssBootstrap="ps-2" nameIcone="arrow-right" />
               </button>
             </div>
 
             <div className="cardApresentacao d-flex flex-column align-items-center col-12 col-md-6 p-3">
-              <img className="w-100" src={imgPessoaJurídica} alt="Sou Pessoa Fisica" />
+              <img
+                className="w-100"
+                src={imgPessoaJurídica}
+                alt="Sou Pessoa Fisica"
+              />
               <button className="d-flex">
                 <Link to="/juridico">Sou Pessoa Juridica</Link>
                 <Icone cssBootstrap="ps-2" nameIcone="arrow-right" />
@@ -64,31 +72,28 @@ export default function Inicio() {
           </div>
 
           <div className="parceiros-inicio d-flex flex-column align-items-center my-5">
-            <TituloDois>
-              Nossos Parceiros
-            </TituloDois>
+            <TituloDois>Nossos Parceiros</TituloDois>
 
-            <div className="swiper d-flex justify-content-between align-items-center mt-2">
-              <div className="swiper-button-prev">
+            <div className="d-flex justify-content-between align-items-center w-100 mt-2">
+              <div className="button-prev">
                 <Icone cssBootstrap="h3" nameIcone="angle-left" />
               </div>
 
               <div
-                className="swiper-wrapper d-flex flex-wrap justify-content-around"
-                style={{ width: "1300px" }}
+                className="d-flex justify-content-around w-100"
               >
-                <span className="cardParceiros">Marca</span>
-                <span className="cardParceiros">Marca</span>
-                <span className="cardParceiros">Marca</span>
-                <span className="cardParceiros">Marca</span>
-                <span className="cardParceiros">Marca</span>
-                <span className="cardParceiros">Marca</span>
+                <span className="cardParceiros py-5 px-5">Marca</span>
+                <span className="cardParceiros d-none d-md-block py-5 px-5">Marca</span>
+                <span className="cardParceiros d-none d-md-block py-5 px-5">Marca</span>
+                <span className="cardParceiros d-none d-lg-block py-5 px-5">Marca</span>
+                <span className="cardParceiros d-none d-xl-block py-5 px-5">Marca</span>
+                <span className="cardParceiros d-none d-xl-block py-5 px-5">Marca</span>
               </div>
 
-              <div className="swiper-button-next">
+              <div className="button-next">
                 <Icone cssBootstrap="h3" nameIcone="angle-right" />
               </div>
-            </div>
+  </div>
           </div>
         </div>
       </section>
@@ -123,8 +128,8 @@ export default function Inicio() {
             </p>
 
             <button className="button-historia d-flex justify-content-center text-white h5">
-              <Link to='/sobre'>Conheça nossa história</Link>
-              <Icone cssBootstrap='ms-2' nameIcone='arrow-right' />
+              <Link to="/sobre">Conheça nossa história</Link>
+              <Icone cssBootstrap="ms-2" nameIcone="arrow-right" />
             </button>
           </div>
         </div>
